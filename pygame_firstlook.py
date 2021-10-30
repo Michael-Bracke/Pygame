da87e76a1890977ba5b36c65ae106bd349477ff6
Michaël Bracké <michael.bracke@becosoft.be>
2021-10-30 18:22:39 +0200

    Pygame/Animatie Lelft <-> right + direction

--- pygame_firstlook.py
+++ pygame_firstlook.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame import image
 from pygame.locals import *
 
 pygame.init()
@@ -26,19 +27,24 @@ bg_img = pygame.image.load('img/sky.png')
 class Player():
     def __init__(self, x, y):
         self.images_right = []
+        self.images_left = []
         self.image_index = 0
         self.counter = 0
         
         for num in range(1,5):
             img_right = pygame.image.load(f'img/guy{num}.png')
             img_right  = pygame.transform.scale(img_right, (40,80))
+            img_left = pygame.transform.flip(img_right, True, False)
             self.images_right.append(img_right)
+            self.images_left.append(img_left)
+
         self.image = self.images_right[self.image_index]
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.vel_y = 0
         self.jumped = False
+        self.direction = 0
 
     def update(self):
         walk_cooldown = 7
@@ -55,23 +61,30 @@ class Player():
         if key[pygame.K_LEFT] == True : 
             dx -= 5
             self.counter += 1
+            self.direction = -1
         if key[pygame.K_RIGHT] == True : 
             dx += 5
             self.counter += 1
+            self.direction = 1
         #counter reset
         if key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] == False:
             self.counter = 0
             self.image_index = 0
-            
+            if self.direction == 1:         
+                self.image = self.images_right[self.image_index]
+            if self.direction == -1:   
+                self.image = self.images_left[self.image_index]
         #de animatie 
         
         if self.counter > walk_cooldown:
             self.counter = 0
             self.image_index += 1
             if self.image_index >= len(self.images_right):
-                self.image_index = 0
-            
-        self.image = self.images_right[self.image_index]
+                self.image_index = 0  
+            if self.direction == 1:         
+                self.image = self.images_right[self.image_index]
+            if self.direction == -1:   
+                self.image = self.images_left[self.image_index]
         #voeg zwaartekracht toe
         self.vel_y += 1
         if self.vel_y > 10:
